914554fb15257ea4f4202fda2273233a0ac7b261
Emanuele Giacomini <giacomini@diag.uniroma1.it>
2025-10-15 00:43:23 +0200

    updates kitty, updated lazyvim and zk

--- .config/nvim/lua/plugins/latex.lua
+++ .config/nvim/lua/plugins/latex.lua
@@ -1,9 +1,10 @@
 return {
   "lervag/vimtex",
   lazy = false,
-  config = function()
+  init = function()
     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable 'K' as it conflicts with LSP hover
     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+    vim.g.vimtex_view_method = "zathura"
   end,
   keys = {
     { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
